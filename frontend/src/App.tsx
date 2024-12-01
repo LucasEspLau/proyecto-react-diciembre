6975d5cbc4bcf5764fd26d1fcd43d9f872b6ba37
LucasEspLau <lukaespinoza12@gmail.com>
2024-12-01 18:57:48 -0500

    subiendo el inicio con tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,14 @@
+import Footer from "./components/Footer"
+import Navbar from "./components/Navbar"
+import Index from "./pages/Index"
 
 function App() {
 
   return (
     <>
+    <Navbar/>
+    <Index/>
+    <Footer/>
     </>
   )
 }
