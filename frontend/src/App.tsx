d382e43453ddc03aa938477326ebb13b54a43443
LucasEspLau <lukaespinoza12@gmail.com>
2024-12-11 22:54:55 -0500

    se añadio  la parte de registro

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar"
 import Index from "./pages/Index"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
+import Registro from "./pages/Registro";
 function App() {
 
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Index/>}/>
           <Route path="/iniciar-sesion" element={<Login/>}/>
+          <Route path="/registro" element={<Registro/>}/>
         </Routes>
         <Footer/>
       </main>
