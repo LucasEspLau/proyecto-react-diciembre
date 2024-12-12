c1863613758e529dc5a5190b3b7cc56931ae3cb6
LucasEspLau <lukaespinoza12@gmail.com>
2024-12-11 21:53:51 -0500

    Se añadio el react router y el api de login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,22 @@
 import Footer from "./components/Footer"
 import Navbar from "./components/Navbar"
 import Index from "./pages/Index"
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
 function App() {
 
   return (
-    <>
-    <Navbar/>
-    <Index/>
-    <Footer/>
-    </>
+    <Router>
+      <main>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Index/>}/>
+          <Route path="/iniciar-sesion" element={<Login/>}/>
+        </Routes>
+        <Footer/>
+      </main>
+
+    </Router>
   )
 }
 
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,20 +1,53 @@
+import { Link } from "react-router-dom";
+
 export default function Navbar(){
-    return(
-        <nav className="bg-red-700 p-4 h-[10vh]">
-            <ul className="flex flex-row items-center justify-around text-white font-extrabold text-md">
-                <li>
-                    <a href="#">Inicio</a>
-                </li>
-                <li>
-                    <a href="#">Sobre Nosotros</a>
-                </li>
-                <li>
-                    <a href="#">Registro</a>
-                </li>
-                <li>
-                    <a href="#">Login</a>
-                </li>
-            </ul>
-        </nav>
-    )
+    const dataUser=localStorage.getItem("datauser")
+    if(!dataUser) {
+        
+        return(
+            <nav className="bg-red-700 p-4 h-[10vh]">
+                <ul className="flex flex-row items-center justify-around text-white font-extrabold text-md">
+                    <li>
+                        <Link to={"/"}>Inicio</Link>
+                    </li>
+                    <li>
+                        <Link to={"/sobre-nosotros"}>Sobre Nosotros</Link>
+                    </li>
+                    <li>
+                        <Link to={"/registro"}>Registró</Link>
+                    </li>
+                    <li>
+                        <Link to={"/iniciar-sesion"}>Iniciar Sesión</Link>
+                    </li>
+                </ul>
+            </nav>
+        )
+    }else{
+        const user=JSON.parse(dataUser)
+        console.log("DATA USER EN NAVBAR",user.nombres)
+        const handleLogout=(e:any)=>{
+            e.preventDefault()
+            localStorage.removeItem("datauser")
+            window.location.href="/"
+        }
+        return(
+            <nav className="bg-red-700 p-4 h-[10vh]">
+                <ul className="flex flex-row items-center justify-around text-white font-extrabold text-md">
+                    <li>
+                        <Link to={"/"}>Inicio</Link>
+                    </li>
+                    <li>
+                        <Link to={"/sobre-nosotros"}>Sobre Nosotros</Link>
+                    </li>
+                    <li>
+                        <button onClick={handleLogout}>Cerrar Sesión</button>
+                    </li>
+
+                </ul>
+            </nav>
+        )
+    }
+  
+
+
 }
