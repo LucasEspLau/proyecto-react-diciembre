2b65bef34c67b07add1527f2668748e8b5579f6f
LucasEspLau <lukaespinoza12@gmail.com>
2024-12-15 18:03:22 -0500

    se actualizo las rutas y se hizo el crud de productos

--- backend/routes.mjs
+++ backend/routes.mjs
@@ -3,7 +3,7 @@ import express from 'express';
 import * as productController from './controllers/productController.mjs'
 */
 import * as UsuarioController from './controller/UsuarioController.mjs'
-
+import * as ProductoController from './controller/ProductoController.mjs'
 const router = express.Router();
 
 /*
@@ -15,5 +15,8 @@ router.delete('/products/:id', productController.deleteProduct);
 router.post('/registro',UsuarioController.createUser);
 router.post('/login', UsuarioController.login);
 
-
+router.post('/crear-producto',ProductoController.createProducto);
+router.put('/actualizar-producto',ProductoController.updateProducto)
+router.get('/traer-productos',ProductoController.getProductos)
+router.delete('/eliminar-producto',ProductoController.deleteProducto)
 export default router;
